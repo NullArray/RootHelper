2f4e774e7272209ebd3221645cb7b172245bb53f
NullArray <opsec@null.net>
2015-11-18 04:05:26 +0100

    Fixed minor syntax error.

--- roothelper.sh
+++ roothelper.sh
@@ -3,31 +3,32 @@
 DONE=$1
 PATH="/tmp/"
 
-if [ "$DONE" == "" ] 
+if [ "$DONE" == "" ]
 then
     echo "Downloading and extracting scripts, when ready execute script with arg $1 to remove files"
     `wget -O /tmp/file1.zip https://github.com/rebootuser/LinEnum/archive/master.zip`                  
     `wget -O /tmp/file2.zip https://github.com/PenturaLabs/Linux_Exploit_Suggester/archive/master.zip`  
     `wget -O /tmp/file3.zip https://github.com/pentestmonkey/unix-privesc-check/archive/1_x.zip`   
-for zip in *.zip
-do
-    dirname=`echo $zip | sed 's/\.zip$//'`
-    if mkdir $dirname
-    then
-        if cd $dirname
+    for zip in *.zip
+    do
+        dirname=`echo $zip | sed 's/\.zip$//'`
+        if mkdir $dirname
         then
-            unzip ../$zip
-        cd ..
-        rm -f $zip 
+            if cd $dirname
+            then
+                unzip ../$zip
+                cd ..
+                rm -f $zip
+            else
+                echo "Could not unpack $zip - cd failed"
+            fi
         else
-            echo "Could not unpack $zip - cd failed"
-        fi
-   	else
             echo "Could not unpack $zip - mkdir failed"
-    fi
-	exit 1
+        fi
+        exit 1
+    done
 else
     echo ""Removing scripts""
     find $PATH/* -exec rm {} \;
     exit 2
-fi 
+fi
